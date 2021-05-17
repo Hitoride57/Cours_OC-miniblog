d668849f6b7378b33b2d58c1fe071ee4c4409c34
Saimonn <simon.tourdot@linuxmail.org>
2021-05-17 22:32:43 +0200

    UPDATE(var_set)
    Ajout fonctionnalité pour choix du nombre d'article à afficher

--- includes/var_set.php
+++ includes/var_set.php
@@ -4,8 +4,15 @@ Fichier de définition des variables récurrentes
 Voir le fichier README.md pour la structure des tables
 */
 
-$db_name = 'test';                       // Nom de la base de données pour le miniblog
-$table_article = 'billets';              // Nom de la table pour le stockage des billets
-$table_comment = 'commentaires';         // Nom de la table pour le stockage des commentaires
-$nb_billet = '5';                        // Nombre de billets affichés par défaut
+$db_name = 'test';                          // Nom de la base de données pour le miniblog
+$table_article = 'billets';                 // Nom de la table pour le stockage des billets
+$table_commentaires = 'commentaires';       // Nom de la table pour le stockage des commentaires
+
+// Nombre de billets affichés par défaut en cas d'absence de choix précédent
+
+if (isset($_POST['nb_billet'])) {
+    $nb_billet = $_POST['nb_billet'];
+} else {
+    $nb_billet = '5';
+}
 ?>
